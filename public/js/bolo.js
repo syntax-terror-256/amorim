1b216bb419f1d2b94a1d7bb0a0499975a11eb6f9
Myckael <matheusmyckael2@gmail.com>
2024-10-10 22:38:21 -0300

    Adicionado o botão de carrinho e ajuste do incremento de quantidade

--- public/js/bolo.js
+++ public/js/bolo.js
@@ -3,18 +3,23 @@ const btnDecrementar = document.getElementById('decrementar');
 const btnIncrementar = document.getElementById('incrementar');
 const inputPeso = document.getElementById('peso');
 
+
 // Função para incrementar o valor
 btnIncrementar.addEventListener('click', () => {
-    let valorAtual = parseInt(inputPeso.value);
+    let valorAtual = parseFloat(inputPeso.value);
     if (valorAtual < 10) { // Limite máximo de 10 kg
-        inputPeso.value = valorAtual + 1;
+        inputPeso.value = (valorAtual + 0.5).toFixed(1);
     }
 });
 
 // Função para decrementar o valor
 btnDecrementar.addEventListener('click', () => {
-    let valorAtual = parseInt(inputPeso.value);
-    if (valorAtual > 1) { // Limite mínimo de 1 kg
-        inputPeso.value = valorAtual - 1;
+    let valorAtual = parseFloat(inputPeso.value);
+    if (valorAtual > 0.5) { // Limite mínimo de 0.5 kg
+        inputPeso.value = (valorAtual - 0.5).toFixed(1);
     }
+});
+// Função para o botão "Adicionar"
+btnAdicionar.addEventListener('click', () => {
+    alert(`Você adicionou ${inputPeso.value} kg de bolo ao carrinho!`);
 });
