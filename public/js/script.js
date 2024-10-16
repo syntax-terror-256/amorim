5496e7f42500c4135ccf5cba9eeed5e82dead226
Pedro Victor <irubydev@gmail.com>
2024-10-15 21:47:19 -0300

    Concluido protótipo das secções Galeria e Sobre nós

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,28 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    let currentImageIndex = 0;
-    const images = document.querySelectorAll('.carousel-image');
-    const totalImages = images.length;
+document.addEventListener('DOMContentLoaded', function() {
+    function createCarousel(carouselId) {
+      const carousel = document.querySelector(`#${carouselId}`);
+      const images = carousel.querySelectorAll('.carousel-image');
+      let currentImageIndex = 0;
+      const totalImages = images.length;
 
-    function showNextImage() {
+      function showNextImage() {
         // Remove a classe "active" da imagem atual
         images[currentImageIndex].classList.remove('active');
-
+        
         // Incrementa o índice da imagem
         currentImageIndex = (currentImageIndex + 1) % totalImages;
-
+        
         // Adiciona a classe "active" à nova imagem
         images[currentImageIndex].classList.add('active');
+      }
+
+      // Altera a imagem automaticamente a cada 3 segundos
+      setInterval(showNextImage, 3000);
     }
 
-    // Altera a imagem automaticamente a cada 3 segundos
-    setInterval(showNextImage, 3000);
-});
+    // Cria carrossel para cada instância
+    createCarousel('carousel1');
+    createCarousel('carousel2');
+    createCarousel('carousel3');
+  });
