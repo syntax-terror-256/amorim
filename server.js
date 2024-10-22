dfca2cb97f34ec2f776fa9b9ff4491c428350386
Hugo Messias <hugomessias9@gmail.com>
2024-10-22 15:23:45 -0300

    Refatorado server.js com Prettier

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import Fastify from "fastify"
+import Fastify from 'fastify'
 import { fileURLToPath } from 'url'
 import path from 'node:path'
-import fs from "node:fs"
+import fs from 'node:fs'
 import fastifyStatic from '@fastify/static'
 import { DataBase } from './database.js'
-
+import fastifyCookie from '@fastify/cookie'
 
 // inicia servidor
-const fastify = Fastify();
+const fastify = Fastify()
 // const server = fastify()
 
 // declara constantes
@@ -15,67 +15,80 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // registra arquivos estáticos
-fastify.register(fastifyStatic , {
-    root: path.join(__dirname, 'public'),
-    prefix: '',
-});
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  prefix: '',
+})
+
+fastify.register(fastifyCookie)
 
 // funções úteis
 function readFile(filePath) {
-    return fs.createReadStream(path.join(__dirname, filePath))
+  return fs.createReadStream(path.join(__dirname, filePath))
 }
 
-// cria rotas
+// rotas para servir páginas html
 fastify.get('/', () => {
-    return readFile('public/index.html')
+  return readFile('public/index.html')
 })
 
 fastify.get('/cardapio', () => {
-    return readFile('public/dynamic.html')
+  return readFile('public/dynamic.html')
 })
 
-fastify.get('/database/ProdutosComuns', async (request, reply) => {
-    try {
-        // retorna o arquivo em cache caso exista
-        await fs.promises.access('cache/ProdutosComuns.json', fs.constants.F_OK)
-        return readFile('cache/ProdutosComuns.json') // TODO: tentar converter para um objeto antes de retornar
-
-    } catch (err) {
-        // caso não exista, consulta o banco de dados e faz o caching
-        const produtosComuns =  await DataBase.getProdutosComuns()
-        console.log('comunicating to server...');
-        await fs.promises.writeFile('cache/ProdutosComuns.json', JSON.stringify(produtosComuns, null, 2))
-        return produtosComuns
+// rotas para comunicação com banco de dados
+fastify.get('/database/ProdutosComuns', async (req, reply) => {
+  try {
+    // retorna o arquivo em cache caso exista
+    await fs.promises.access('cache/ProdutosComuns.json', fs.constants.F_OK)
+    return readFile('cache/ProdutosComuns.json') // TODO: tentar converter para um objeto antes de retornar
+  } catch (err) {
+    // caso não exista, consulta o banco de dados e faz o caching
+    if (err.code === 'ENOENT') {
+      const produtosComuns = await DataBase.getProdutosComuns()
+      await fs.promises.writeFile(
+        'cache/ProdutosComuns.json',
+        JSON.stringify(produtosComuns, null, 2)
+      )
+      return reply.send(produtosComuns)
     }
+  }
 })
 
-fastify.get('/database/CategoriasComuns', async (request, reply) => {
-    try {
-        // retorna o arquivo em cache caso exista
-        await fs.promises.access('cache/CategoriasComuns.json', fs.constants.F_OK)
-        return readFile('cache/CategoriasComuns.json')
-
-    } catch (err) {
-        console.log(err)
-        // caso não exista, consulta o banco de dados e faz o caching
-        const categoriasComuns =  await DataBase.getCategoriasComuns()
-        console.log('comunicating to server...');
-        await fs.promises.writeFile('cache/CategoriasComuns.json', JSON.stringify(categoriasComuns, null, 2))
-        return categoriasComuns
+fastify.get('/database/CategoriasComuns', async (req, reply) => {
+  try {
+    // retorna o arquivo em cache caso exista
+    await fs.promises.access('cache/CategoriasComuns.json', fs.constants.F_OK)
+    return readFile('cache/CategoriasComuns.json')
+  } catch (err) {
+    // caso não exista, consulta o banco de dados e faz o caching
+    if (err.code === 'ENOENT') {
+      const categoriasComuns = await DataBase.getCategoriasComuns()
+      await fs.promises.writeFile(
+        'cache/CategoriasComuns.json',
+        JSON.stringify(categoriasComuns, null, 2)
+      )
+      return reply.send(categoriasComuns)
     }
+  }
 })
 
-fastify.get('/database/cache', async (request, reply) => {
-    const produtosComuns = await DataBase.getProdutosComuns()
-    const categoriasComuns = await DataBase.getCategoriasComuns()
-    await fs.promises.writeFile('cache/CategoriasComuns.json', JSON.stringify(categoriasComuns, null, 2))
-    await fs.promises.writeFile('cache/ProdutosComuns.json', JSON.stringify(produtosComuns, null, 2))
-    return
+fastify.get('/database/cache', async (req, reply) => {
+  const produtosComuns = await DataBase.getProdutosComuns()
+  const categoriasComuns = await DataBase.getCategoriasComuns()
+  await fs.promises.writeFile(
+    'cache/CategoriasComuns.json',
+    JSON.stringify(categoriasComuns, null, 2)
+  )
+  await fs.promises.writeFile(
+    'cache/ProdutosComuns.json',
+    JSON.stringify(produtosComuns, null, 2)
+  )
+  reply.status(201)
 })
-
 
 // especifica endereço e porta do servidor
 fastify.listen({
-    host: '0.0.0.0',
-    port: process.env.PORT ?? 3333
-  })
+  host: '0.0.0.0',
+  port: process.env.PORT ?? 3333,
+})
