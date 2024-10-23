5d11928396b3cf9b057f8b34f4cd8052bac195eb
Hugo Messias <hugomessias9@gmail.com>
2024-10-23 16:59:17 -0300

    Modificado 'no-unused-vars' para 'warn' ao invés de 'error'

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   pluginJs.configs.recommended,
-];
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+]
